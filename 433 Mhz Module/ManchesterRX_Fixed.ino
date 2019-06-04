6e3e8f489a35af36688edc36a4954cc6c2c8548d
will7007 <38322012+will7007@users.noreply.github.com>
2019-06-04 16:58:01 -0400

    Receiving Arduino code
    Thanks to https://www.instructables.com/id/Attiny85-RF-Transmitter-to-Arduino-Uno-Receiver-Ma/ and http://mchr3k.github.io/arduino-libs-manchester/ (official documentation) for providing code which fixes the example receiving code.

---
+++ 433 Mhz Module/ManchesterRX_Fixed.ino
@@ -0,0 +1,37 @@
+#include <Manchester.h>
+
+/*
+
+Manchester Receiver example
+
+  In this example receiver will receive one 16 bit number per transmittion
+
+Try different speeds using this constants, your maximum possible speed will 
+
+  depend on various factors like transmitter type, distance, microcontroller speed, ...
+
+  MAN_300 0
+  MAN_600 1
+  MAN_1200 2
+  MAN_2400 3
+  MAN_4800 4
+  MAN_9600 5
+  MAN_19200 6
+  MAN_38400 7
+
+*/
+
+#define RX_PIN 7
+void setup() {
+  Serial.begin(9600);
+  man.setupReceive(RX_PIN, MAN_1200);
+  man.beginReceive();
+}
+
+void loop() {
+  if (man.receiveComplete()) {
+    uint16_t m = man.getMessage();
+    Serial.println(m);
+    man.beginReceive(); //start listening for next message right after you retrieve the message
+  }
+}

04e6b17017ee13a97447cd0d80df2e65a04bc930
will7007 <38322012+will7007@users.noreply.github.com>
2019-06-11 16:14:14 -0400

    Minor transmission code edits

--- 433 Mhz Module/433Tx_Tiny_BME280.ino
+++ 433 Mhz Module/433Tx_Tiny_BME280.ino
@@ -1,9 +1,11 @@
-//Works at 1Mhz
+//Works at 1Mhz, up to about 116 feet in open space
 #include <TinyWireM.h>
 #include <Manchester.h>
 #include "CORE_BME280_I2C.h"
  
 BME280_I2C sensebme; // Set to used address 0x77 as default // or BME280_I2C bme(0x76); // I2C using address 0x76
+const int sensorAddress = 0xA;
+int temp, humid = 0;
  
 void setup() {
  man.setupTransmit(4, MAN_300);
@@ -13,10 +15,17 @@ if (!sensebme.begin()) {while (1);}
 }
  
 void loop() {
-sensebme.readSensor();
-man.transmit((int)sensebme.getTemperature_F());
-man.transmit((int)sensebme.getHumidity());
-delay(3000); 
+  sensebme.readSensor();
+  temp = (int)sensebme.getTemperature_F();
+  humid = (int)sensebme.getHumidity();
+
+  //for(int i=0;i<=10;i++) {
+    man.transmit(sensorAddress);
+    man.transmit(temp);
+    man.transmit(humid);
+  //}
+  
+  delay(1000); 
 }
 
 void forcedMode() { //tell the sensor to take a reading and then go back to sleep

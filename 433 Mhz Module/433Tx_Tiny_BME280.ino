df6ef70a077339a01946bd85152740ecca3beaa2
will7007 <38322012+will7007@users.noreply.github.com>
2019-06-14 14:03:04 -0400

    Slowed down transmit speed, added data identifiers
    I slowed down the transmit speed so that other sensors could have time to receive the signal and then send it out again without missing any packets. I also removed the forcedMeasurement() method as it wasn't being used, but I will likely need it in the future.

--- 433 Mhz Module/433Tx_Tiny_BME280.ino
+++ 433 Mhz Module/433Tx_Tiny_BME280.ino
@@ -1,16 +1,15 @@
-//Works at 1Mhz, up to about 116 feet in open space
+//Works at 1Mhz
 #include <TinyWireM.h>
 #include <Manchester.h>
 #include "CORE_BME280_I2C.h"
  
-BME280_I2C sensebme; // Set to used address 0x77 as default // or BME280_I2C bme(0x76); // I2C using address 0x76
+BME280_I2C sensebme;
 const int sensorAddress = 0xA;
 int temp, humid = 0;
  
 void setup() {
  man.setupTransmit(4, MAN_300);
 
- 
 if (!sensebme.begin()) {while (1);}
 }
  
@@ -18,20 +17,18 @@ void loop() {
   sensebme.readSensor();
   temp = (int)sensebme.getTemperature_F();
   humid = (int)sensebme.getHumidity();
-
-  //for(int i=0;i<=10;i++) {
-    man.transmit(sensorAddress);
-    man.transmit(temp);
-    man.transmit(humid);
-  //}
+  
+  man.transmit(0xAA);
+  delay(500);
+  man.transmit(sensorAddress);
+  delay(500);
+  man.transmit(0xBB);
+  delay(500);
+  man.transmit(temp);
+  delay(500);
+  man.transmit(0xCC);
+  delay(500);
+  man.transmit(humid);
   
   delay(1000); 
-}
-
-void forcedMode() { //tell the sensor to take a reading and then go back to sleep
-    TinyWireM.beginTransmission(119); 
-    TinyWireM.write(0xF4);                             // ctrl_meas
-    TinyWireM.write(0b00100010);                       // Temp sampling 1, pressure sampling 0 (no reading), forced mode
-    TinyWireM.endTransmission();
-    delay(100); //a built-in delay to give the sensor time to read the value and go to sleep again
 }

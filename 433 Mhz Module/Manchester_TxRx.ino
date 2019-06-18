aad7d35381ee56d2088687c39ef6650513b6c2a8
will7007 <38322012+will7007@users.noreply.github.com>
2019-06-18 16:02:04 -0400

    Added receiving buffer and transmit triggering button

--- 433 Mhz Module/Manchester_TxRx.ino
+++ 433 Mhz Module/Manchester_TxRx.ino
@@ -1,31 +1,47 @@
 #include <Manchester.h>
 
- 
+uint16_t buffering[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
+int counter, timer = 0;
+bool led = false;
 void setup() {
-  man.setupReceive(12, MAN_300);
-  man.setupTransmit(6, MAN_300);
+  man.setupReceive(11, MAN_300);
+  man.setupTransmit(5, MAN_300);
   man.beginReceive();
 
   pinMode(13,OUTPUT);
-//  pinMode(12,INPUT);
-  pinMode(10,OUTPUT);
+  pinMode(7,INPUT);
 
-  digitalWrite(13,HIGH);
-  digitalWrite(10,LOW);
+  pinMode(12,OUTPUT);
+  pinMode(9,OUTPUT);
 
-  pinMode(5,OUTPUT);
+  digitalWrite(12,HIGH);
+  digitalWrite(9,LOW);
+
   pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
   
-  digitalWrite(5,HIGH);
-  digitalWrite(4,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
   Serial.begin(9600);
 }
  
 void loop() {
   if (man.receiveComplete()) {
-    uint16_t m = man.getMessage();
-    man.transmit(m);
-    Serial.println(m);
+    buffering[counter] = man.getMessage();
+    Serial.print((char)buffering[counter]);Serial.print(" counter: "); Serial.println(counter);
+    digitalWrite(13, led);
+    counter++;
     man.beginReceive();
+  }
+  
+  if (counter > 25) counter = 0;
+  
+  if (digitalRead(7)) {
+    delay(10); //delay to give transmitter time just in case
+    Serial.println("Sending...");
+    digitalWrite(13,HIGH);
+    for(int i = 0;i < 25; i++) man.transmit(buffering[i] + 32);
+    Serial.println("Sent!");
+    digitalWrite(13,LOW);
   }
 }

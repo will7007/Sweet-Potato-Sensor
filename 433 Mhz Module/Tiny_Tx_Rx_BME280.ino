3dc3c4d9c8af9765d4fef51a885f7d8b26319a18
will7007 <38322012+will7007@users.noreply.github.com>
2019-07-02 16:12:56 -0400

    Shortened/added delays for sending

--- 433 Mhz Module/Tiny_Tx_Rx_BME280.ino
+++ 433 Mhz Module/Tiny_Tx_Rx_BME280.ino
@@ -20,7 +20,7 @@ void setup() {
   if (!sensebme.begin()) {
     while(1) {
     man.transmit(5);
-    delay(10);
+    delay(100);
     }
   }
 }
@@ -31,13 +31,14 @@ void loop() {
     if(buffering[counter] == 4) eof = true;
     counter++;
     man.beginReceive();
+    
   }
   
   if (counter > 6) counter = 0;
   
-  if (digitalRead(BUTTON) }} eof) {
+  if (digitalRead(BUTTON) || eof) {
     man.stopReceive(); //must include this to transmit
-    
+
     sensebme.readSensor();
     temp = (int)sensebme.getTemperature_F();
     humid = (int)sensebme.getHumidity();
@@ -48,21 +49,24 @@ void loop() {
     if(eof) delay(1000); //extra delay for auto-send
     
     man.transmit(1); //hex values sent out seem to cause issues on the receiving end,
-    delay(200);      //particularly when printing out what is received to the serial monitor
+    delay(50);      //particularly when printing out what is received to the serial monitor
     man.transmit(ADDRESS);
-    delay(200);
+    delay(50);
     man.transmit(2);
-    delay(200);
+    delay(50);
     man.transmit(temp);
-    delay(200);
+    delay(50);
     man.transmit(3);
-    delay(200);
+    delay(50);
     man.transmit(humid);
-    delay(200);
+    delay(50);
     man.transmit(4);
-    delay(200);
+    delay(50);
     
-    for(int i = 0;i <= 6; i++) man.transmit(buffering[i]);
+    for(int i = 0;i <= 6; i++) {
+      man.transmit(buffering[i]);
+      delay(50); //not sure we even need this delay but we might as well include it just in case
+    }
     
     counter = 0;
     eof = false;

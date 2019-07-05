feeabb5f7d41c8c1cec883d74057c6290a4daeda
will7007 <38322012+will7007@users.noreply.github.com>
2019-07-05 16:21:44 -0400

    Base station now transmits/receives arrays of bytes
    I have not tested the fake sensor data transmitting portion of the code, but it should work as it came from the previous single-byte version of this code.
    Turns out the example wasn't broken after all--you just need to include the number of bytes being sent (including the size byte) in the first element of the transmitted array.

--- 433 Mhz Module/Base_Station.ino
+++ 433 Mhz Module/Base_Station.ino
@@ -1,78 +1,78 @@
-#include <Manchester.h>
+//Run on an AtMEGA2560
+#include "Manchester.h"
 
-//int dataNumber = 0;
-//int counter = 0;
+//Transmission keywords
+//0: Reserved
+//1: Address
+//2: Temperature
+//3: Humidity
+//4: EOF
+//5: Sensor error
+//6: Nominal temp/humidity, even address following (we may only need one of these if there is a checksum in a data array)
+//7: Nominal temp/humidity, odd address following
+//8: Low battery voltage
+//9: Reserved
+//10 >=: Standard numbers
 
-#define RX_PIN 12
-void setup() {
+#define RX_PIN 11
+#define LED_PIN 13
+
+uint8_t moo = 1;
+#define BUFFER_SIZE 22
+uint8_t buffer[BUFFER_SIZE];
+const uint8_t FAKE_DATA[4] = {4,13,20,30};
+
+void setup() 
+{
+  pinMode(LED_PIN, OUTPUT);  
+  digitalWrite(LED_PIN, moo);
   Serial.begin(9600);
   man.setupReceive(RX_PIN, MAN_300);
   man.setupTransmit(14, MAN_300);
-  man.beginReceive();
+  man.beginReceiveArray(BUFFER_SIZE, buffer);
 
   //Receiver pins
-  pinMode(13,OUTPUT);
-  pinMode(12,INPUT);
-  pinMode(10,OUTPUT);
-
-  digitalWrite(13,HIGH);
-  digitalWrite(10,LOW);
+  pinMode(12,OUTPUT);
+  pinMode(9,OUTPUT);
+  
+  digitalWrite(12,HIGH);
+  digitalWrite(9,LOW);
 
   //Transmitter pins
   pinMode(15,OUTPUT);
   pinMode(16,OUTPUT);
 
   digitalWrite(15,HIGH);
-  digitalWrite(16,LOW);
+  digitalWrite(16,LOW);  
 }
 
-void loop() {
-  if (man.receiveComplete()) {
-    uint16_t m = man.getMessage();
-    switch (m) {
-      case 1: 
-      Serial.print("Address: ");
-      break;
-      case 2:
-      Serial.print("Temperature: ");
-      break;
-      case 3:
-      Serial.print("Humidity: ");
-      break;
-      case 4:
-      Serial.println("End of transmission\n");
-      break;
-      case 5:
-      Serial.print("Sensor setup error");
-      break;
-      default:
-      Serial.println(m);
-      break;
-    }
-    man.beginReceive(); //start listening for next message right after you retrieve the message
+void loop() 
+{
+  if (man.receiveComplete()) 
+  {
+    uint8_t receivedSize = 0;
+
+    //do something with the data in 'buffer' here before you start receiving to the same buffer again
+    receivedSize = buffer[0];
+    for(uint8_t i=1; i<receivedSize; i++)
+      Serial.println(buffer[i]);
+    
+    Serial.println();
+
+    man.beginReceiveArray(BUFFER_SIZE, buffer);
+    moo = ++moo % 2; //FIXME: Remove this ugly example code
+    digitalWrite(LED_PIN, moo);
   }
+  
   else if (Serial.available() > 0) {  //comment out for just receiving
-    Serial.read();
+    Serial.read(); //throw away whatever character we got
     delay(10);
     man.stopReceive(); //must not be receiving if you want to transmit
     Serial.print("Transmitting... ");
-      
-    //fake test sensor data
-    man.transmit(1); 
-    delay(200);      
-    man.transmit(10);
-    delay(200);
-    man.transmit(2);
-    delay(200);
-    man.transmit(20);
-    delay(200);
-    man.transmit(3);
-    delay(200);
-    man.transmit(30);
-    delay(200);
-    man.transmit(4);
+
+    man.transmitArray(4,FAKE_DATA);
     
     Serial.println("Done.");
-    man.beginReceive();
+    man.beginReceiveArray(BUFFER_SIZE, buffer);
   }
 }

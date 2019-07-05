0eb996f4c420aeb1330154b33686b35e295bd17b
will7007 <38322012+will7007@users.noreply.github.com>
2019-07-05 16:12:55 -0400

    Transmit function changed to byte array
    Do not use man.workAround1MhzTinyCore(), it will cause the program to transmit the array too quickly (likely 8x as fast). Also, make sure the first byte of your data array is the number of bytes that will be transmitted.

--- 433 Mhz Module/Tiny_Tx_Rx_BME280.ino
+++ 433 Mhz Module/Tiny_Tx_Rx_BME280.ino
@@ -1,75 +1,49 @@
 #include <Manchester.h>
 #include "CORE_BME280_I2C.h"
 
-uint16_t buffering[7] = {0,0,0,0,0,0,0}; 
-int temp,humid,counter = 0;
-bool eof = false;
-
 const int BUTTON = 1; //button pin
 const int ADDRESS = 60;
+const int LENGTH = 4;
 
+uint8_t buffer[LENGTH];
+uint8_t transmitBuffer[7] = {7,ADDRESS,1,2,6,7,8};
 BME280_I2C sensebme;
 
 void setup() {
+//  man.workAround1MhzTinyCore(); DO NOT USE THIS FUNCTION! It makes the program transmit too fast
   man.setupReceive(4, MAN_300);
   man.setupTransmit(3, MAN_300);
-  man.beginReceive();
+  man.beginReceiveArray(LENGTH, buffer);
 
-  pinMode(BUTTON,INPUT);
+  pinMode(BUTTON,OUTPUT);
 
   if (!sensebme.begin()) {
     while(1) {
-    man.transmit(5);
-    delay(100);
+      man.transmit(5);
+      delay(100);
     }
   }
 }
  
 void loop() {
-  if (man.receiveComplete()) {
-    buffering[counter] = man.getMessage();
-    if(buffering[counter] == 4) eof = true;
-    counter++;
-    man.beginReceive();
-    
-  }
-  
-  if (counter > 6) counter = 0;
-  
-  if (digitalRead(BUTTON) || eof) {
-    man.stopReceive(); //must include this to transmit
+  if (man.receiveComplete()) { //|| digitalRead(BUTTON)
+    man.stopReceive(); 
+    delay(100);
+
+    transmitBuffer[4] = buffer[1]; //address
+    transmitBuffer[5] = buffer[2]; //temp
+    transmitBuffer[6] = buffer[3]; //humidity
 
     sensebme.readSensor();
-    temp = (int)sensebme.getTemperature_F();
-    humid = (int)sensebme.getHumidity();
+    
+    transmitBuffer[2] = (int)sensebme.getTemperature_F();
+    transmitBuffer[3] = (int)sensebme.getHumidity();
 
-    if(temp <= 5) temp = 6;
-    if(humid <= 5) humid = 6;
+    if(transmitBuffer[2] <= 9) transmitBuffer[2] = 10;
+    if(transmitBuffer[3] <= 9) transmitBuffer[3] = 10;
 
-    if(eof) delay(1000); //extra delay for auto-send
-    
-    man.transmit(1); //hex values sent out seem to cause issues on the receiving end,
-    delay(50);      //particularly when printing out what is received to the serial monitor
-    man.transmit(ADDRESS);
-    delay(50);
-    man.transmit(2);
-    delay(50);
-    man.transmit(temp);
-    delay(50);
-    man.transmit(3);
-    delay(50);
-    man.transmit(humid);
-    delay(50);
-    man.transmit(4);
-    delay(50);
-    
-    for(int i = 0;i <= 6; i++) {
-      man.transmit(buffering[i]);
-      delay(50); //not sure we even need this delay but we might as well include it just in case
-    }
-    
-    counter = 0;
-    eof = false;
-    man.beginReceive();
+    man.transmitArray(7,transmitBuffer);
+
+    man.beginReceiveArray(LENGTH, buffer);
   }
 }
